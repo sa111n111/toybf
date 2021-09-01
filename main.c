bdb3a861be2efd493ea501e571096c7dc24d3571
Sutinder S. Saini <sutinder456@gmail.com>
2021-09-01 16:04:44 -0700

    first commit

---
+++ main.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdint.h>
+
+char tape[3000]; 
+char *data_pointer;
+char *program_pointer;
+
+void parse_brainfuck(char* program) {
+    int loop_trigger;
+    data_pointer = tape;
+    program_pointer = program;
+
+    while(*program_pointer) {
+        switch(*program_pointer) {
+            case '>':
+            data_pointer++;
+            break;
+
+            case '<':
+            data_pointer--;
+            break;
+
+            case '+':
+            (*data_pointer)++;
+            break;
+
+            case '-':
+            (*data_pointer)--;
+            break;
+
+            case '.':
+            printf("%c", *data_pointer);
+            break;
+
+            case ',':
+            getchar();
+            break;
+
+            case '[':
+            loop_trigger = 1;
+
+            while(loop_trigger != 0) {
+                switch(*program_pointer) {
+                    case '[': loop_trigger++;
+                    case ']': loop_trigger--;
+                }
+                *program_pointer++;
+            }
+            break;
+
+            case ']':
+            loop_trigger = 1;
+            while(loop_trigger != 0) {
+                switch(*program_pointer) {
+                    case '[': loop_trigger++;
+                    case ']': loop_trigger--;
+                }
+                *program_pointer--;
+            }
+            break;
+        }
+
+       program_pointer++;
+    }
+}
+
+int main() {
+    parse_brainfuck(">++++++++[<+++++++++>-]<.>++++[<+++++++>-]<+.+++++++..+++.>>++++++[<+++++++>-]<++.------------.>++++++[<+++++++++>-]<+.<.+++.------.--------.>>>++++[<++++++++>-]<+.");
+}
